4cc9ffa762caf743fda45d507435d4f34144388c
mdababi1989 <mdababi1989@gmail.com>
2023-09-04 23:28:03 +0100

    add repositories

---
+++ src/main/java/com/driveease/rentals/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.driveease.rentals.repositories;
+
+import com.driveease.rentals.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
